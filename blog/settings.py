55154c1ca7054d1d96c79c5f4469a5c70c969dcb
lucas tagliapietra <lucastagliapietra@hotmail.com>
2023-01-10 18:57:44 -0300

    static

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recetas.RecetasConfig',
+    'recetas.apps.RecetasConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static' )
+STATIC_URL = 'static/'
