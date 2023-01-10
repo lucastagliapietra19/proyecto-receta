a1ea77ae5856399eb4d468e77ea60c5137de6525
lucas tagliapietra <lucastagliapietra@hotmail.com>
2023-01-10 20:31:59 -0300

    actualizacion

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from recetas.views import index
+from recetas.views import index, PostList, PostCrear
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recetas/', index),
+    path('recetas/', index, name="recetas-index"),
+    path("recetas/listar/",PostList.as_view(), name="recetas-listar"),
+    path('recetas/crear/',PostCrear.as_view(), name="recetas-crear"),
 
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView
+from recetas.models import Post
+from django.urls import reverse_lazy
 def index(request):
     return render(request, "recetas/index.html",{} )
+
+class PostList(ListView):
+    model= Post
+    template_name = "/recetas/post_list.html"
+
+class PostCrear(CreateView):
+    model = Post
+    success_url =reverse_lazy("recetas-listar")
+    fields = '__all__'
